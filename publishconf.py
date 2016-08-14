1cb5e105d6ff7bf9efb495ec5e3c2b93c3e32060
TinaHeiligers <tina.heiligers@gmail.com>
2016-08-14 09:23:18 -0700

    changed origin to publish in Makefile

--- publishconf.py
+++ publishconf.py
@@ -11,7 +11,8 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://www.tinkerantreats.com'
+# SITEURL = 'http://www.tinkerantreats.com'
+SITEURL = 'https://github.com/TinaHeiligers/tinaheiligers.github.io'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
