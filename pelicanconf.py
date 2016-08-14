26cd6baf9f837e8347bd93c872e4c61dda9a2cca
TinaHeiligers <tina.heiligers@gmail.com>
2016-08-14 13:57:21 -0700

    changes some settings in pelicanconf.py file

--- pelicanconf.py
+++ pelicanconf.py
@@ -5,13 +5,14 @@ from __future__ import unicode_literals
 AUTHOR = u'Tina H'
 SITENAME = u'Tinker an Treats'
 SITEURL = ''
-
+# GITHUB_URL = 'http://github.com/TinaHeiligers/'
 PATH = 'content'
 
 TIMEZONE = 'America/Phoenix'
 
 DEFAULT_LANG = u'en'
-
+DEFAULT_PAGINATION = 4
+REVERSE_CATEGORY_ORDER = True
 
 DISPLAY_PAGES_ON_MENU = True
 
@@ -25,16 +26,20 @@ AUTHOR_FEED_RSS = None
 # Blogroll
 LINKS = (('Pelican', 'http://getpelican.com/'),
          ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+         ('Jinja2', 'http://jinja.pocoo.org/'))
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
-
-DEFAULT_PAGINATION = 5
+SOCIAL = (('twitter', 'http://twitter.com/tinaheiligers1'),
+          ('LinkedIn', 'https://www.linkedin.com/in/christianeheiligers'),)
 
 SUMMARY_MAX_LENGTH = 20
 
+STATIC_PATHS = ['extra/CNAME']
+EXTRA_PATH_METADATA = {
+  'extra/CNAME':{'path':'CNAME'}
+}
+PYGMENTS_RST_OPTIONS = {'linenos': 'table'}
+
+CSS_FILE = 'main.css'
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
